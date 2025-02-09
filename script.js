149fce3c3f2df98767e16da9a4b423cb5d3a749e
Akshay <98652196+akshayg-08@users.noreply.github.com>
2025-02-09 21:59:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var mytable = document.getElementById("mytable")
+var table = document.getElementById("table")
+var tt = document.getElementById("tt")
+tt.addEventListener("click",function(){
+  table.style.display = "block"
+})
